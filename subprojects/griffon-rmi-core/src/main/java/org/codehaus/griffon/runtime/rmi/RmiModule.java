5af954041ab20c31d6636db3bd1c37681a818d48
Andres Almiray <aalmiray@gmail.com>
2021-03-02 02:00:49 +0100

    Build updates

--- subprojects/griffon-rmi-core/src/main/java/org/codehaus/griffon/runtime/rmi/RmiModule.java
+++ subprojects/griffon-rmi-core/src/main/java/org/codehaus/griffon/runtime/rmi/RmiModule.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2014-2020 The author and/or original authors.
+ * Copyright 2014-2021 The author and/or original authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import griffon.plugins.rmi.RmiClientFactory;
 import griffon.plugins.rmi.RmiClientStorage;
 import griffon.plugins.rmi.RmiHandler;
 import org.codehaus.griffon.runtime.core.injection.AbstractModule;
-import org.kordamp.jipsy.ServiceProviderFor;
+import org.kordamp.jipsy.annotations.ServiceProviderFor;
 
 import javax.inject.Named;
 
